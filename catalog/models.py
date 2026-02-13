78c7c0b9bf57a91cae5e296d57ac9a825fed687f
Roman <lapka.roman@yandex.ru>
2026-02-13 15:28:46 +0500

    Initial commit

---
+++ catalog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.PROTECT,
+        related_name="books"
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.PROTECT,
+        related_name="books"
+    )
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
